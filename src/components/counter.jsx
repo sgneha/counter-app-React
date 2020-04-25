99b7bae6128c3aa4da61931f6cc8bb5ad36b5fd2
sgneha <sgneha05@gmail.com>
2020-04-25 13:58:24 +0100

    using usestate

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,13 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React, { useState } from "react";
 const Counter = () => {
+  const [count, setCount] = useState(0);
   return (
-    <div>
-      <h1>Hello World</h1>
-      <button>Increment</button>
-    </div>
+    //changed div to this  such to remoove extra div (can check in inspect).(React.Fragment is child of object React we imported above)
+    <React.Fragment>
+      <span>{count === 0 ? "Zero" : count}</span>
+      <button OnClick={() => setCount(count + 1)}>Increment</button>
+    </React.Fragment>
   );
 };
 
