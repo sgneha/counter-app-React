21ddeabb4aaf75c691ca98fbfbb41876703c4346
sgneha <sgneha05@gmail.com>
2020-04-25 14:41:59 +0100

    added image

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,10 +2,12 @@
 import React, { useState } from "react";
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [imageURL, setImageURL] = useState("https://picsum.photos/200");
 
   return (
     //changed div to this  such to remoove extra div (can check in inspect).(div is child of object React we imported above)
     <div>
+      <img src={imageURL} />
       <span>{format(count)}</span>
       <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
