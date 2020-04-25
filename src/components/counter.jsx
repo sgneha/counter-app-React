eb0dab4cf91ceb231fa1d0b5e6f22ee7845e7ca6
sgneha <sgneha05@gmail.com>
2020-04-25 14:56:39 +0100

    added inline style

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,7 +8,9 @@ const Counter = () => {
     //changed div to this  such to remoove extra div (can check in inspect).(div is child of object React we imported above)
     <div>
       <img src={imageURL} />
-      <span className="badhe badge-primary m-2">{format(count)}</span>
+      <span style={{ fontSize: 30 }} className="badhe badge-primary m-2">
+        {format(count)}
+      </span>
       <button
         className="btn btn-secondry btn-sm"
         onClick={() => setCount(count + 1)}
