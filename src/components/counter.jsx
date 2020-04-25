7469da1005f6a1f503ea5022e9db9f6d34a9aa34
sgneha <sgneha05@gmail.com>
2020-04-25 14:22:44 +0100

    changes OnClick to onClick,now working  and extracted to format method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,13 +2,17 @@
 import React, { useState } from "react";
 const Counter = () => {
   const [count, setCount] = useState(0);
+
   return (
-    //changed div to this  such to remoove extra div (can check in inspect).(React.Fragment is child of object React we imported above)
-    <React.Fragment>
-      <span>{count === 0 ? "Zero" : count}</span>
-      <button OnClick={() => setCount(count + 1)}>Increment</button>
-    </React.Fragment>
+    //changed div to this  such to remoove extra div (can check in inspect).(div is child of object React we imported above)
+    <div>
+      <span>{format(count)}</span>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
   );
 };
+function format(count) {
+  return count === 0 ? "Zero" : count;
+}
 
 export default Counter;
