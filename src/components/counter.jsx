a820fafaa068d3147583ab5e261f5300b785d263
sgneha <sgneha05@gmail.com>
2020-04-25 19:06:02 +0100

    deleted unrequired code for demonstrating conditional rendering first method=>make a method Tag and call that

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,36 +3,22 @@ import React, { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
-  const [imageURL, setImageURL] = useState("https://picsum.photos/200");
   const [tags, setTag] = useState(["tag1", "tag2"]);
 
   return (
     //changed div to this  such to remoove extra div (can check in inspect).(div is child of object React we imported above)
-    <div>
-      <img src={imageURL} />
-      <span className={getBadgeclasses(count)}>{format(count)}</span>
-      <button
-        className="btn btn-secondry btn-sm"
-        onClick={() => setCount(count + 1)}
-      >
-        Increment
-      </button>
-      <ul>
-        {tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    </div>
+    <div>{Tag(tags)}</div>
   );
 };
-function getBadgeclasses(count) {
-  let classes = "badge m-2 badge-";
-  classes += count === 0 ? "warning" : "primary";
-  return classes;
-}
-
-function format(count) {
-  return count === 0 ? "Zero" : count;
+function Tag(tags) {
+  if (tags.length === 0) return <p>There are no tags</p>;
+  return (
+    <ul>
+      {tags.map((tag) => (
+        <li key={tag}>{tag}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default Counter;
