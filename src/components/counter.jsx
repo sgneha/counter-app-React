c3e04b49aa77380d51e615ad3cbc8f05217901d2
sgneha <sgneha05@gmail.com>
2020-04-25 15:12:20 +0100

    added bootstrap badge yellow on condition

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,16 +1,17 @@
 // import React, { Component } from "react";
 import React, { useState } from "react";
+
 const Counter = () => {
   const [count, setCount] = useState(0);
   const [imageURL, setImageURL] = useState("https://picsum.photos/200");
+  let classes = "badge m-2 badge-";
+  classes += count === 0 ? "warning" : "primary";
 
   return (
     //changed div to this  such to remoove extra div (can check in inspect).(div is child of object React we imported above)
     <div>
       <img src={imageURL} />
-      <span style={{ fontSize: 30 }} className="badhe badge-primary m-2">
-        {format(count)}
-      </span>
+      <span className={classes}>{format(count)}</span>
       <button
         className="btn btn-secondry btn-sm"
         onClick={() => setCount(count + 1)}
