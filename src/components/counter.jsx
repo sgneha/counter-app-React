b7b528343508fe50f6a2f70193563ac7a2f0ac34
sgneha <sgneha05@gmail.com>
2020-04-25 15:17:16 +0100

    refactored-dierect called getBadge classes from span

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,13 +4,12 @@ import React, { useState } from "react";
 const Counter = () => {
   const [count, setCount] = useState(0);
   const [imageURL, setImageURL] = useState("https://picsum.photos/200");
-  let classes = getBadgeclasses(count);
 
   return (
     //changed div to this  such to remoove extra div (can check in inspect).(div is child of object React we imported above)
     <div>
       <img src={imageURL} />
-      <span className={classes}>{format(count)}</span>
+      <span className={getBadgeclasses(count)}>{format(count)}</span>
       <button
         className="btn btn-secondry btn-sm"
         onClick={() => setCount(count + 1)}
